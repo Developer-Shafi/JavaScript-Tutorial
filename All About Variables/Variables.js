feced9df89c2e2294b1e300e6444264dc664536d
Developer-Shafi <shafitechdeveloper1133@gmail.com>
2024-06-12 00:01:57 +0500

    All about variable

---
+++ All About Variables/Variables.js
@@ -0,0 +1,11 @@
+//const name = "Shafi"
+let cast = "cheema"
+var city = "khairpur"
+{
+    let cast = "Malik"
+    var city = "karachi"
+    console.log(cast)
+    console.log(city)
+}
+console.log(cast)
+console.log(city)
