5625c5dcb2a618f5dcd4cf0ecec82ebd3eb671d6
Developer-Shafi <shafitechdeveloper1133@gmail.com>
2024-06-13 23:27:14 +0500

    Today's lecture onf Datatypes in javaScript

---
+++ DataTypes in JavaScript/index.js
@@ -0,0 +1,34 @@
+// String, Number,BigInt,Null,undefined,Symbol,
+// let name = "Ahmed"
+// let Score = 444.3
+// let Bignumber = 32188588848893829447n
+// // let Temprature = Null
+// let gameCounter = undefined
+// let button = Symbol("123")
+// let loginbutton = Symbol("123")
+// console.log(Symbol("123") === Symbol("123"))
+
+// Non primitive Datatypes in JavaScript
+
+// Object, Function, Array
+
+// Array
+let lovebirds = ["Muzamil,Khizra","humair,Malah","Yasir, Amal"]
+
+// Function
+const Addition =(a,b)=>{
+return a + b;
+}
+console.log(lovebirds)
+console.log(Addition(22,33))
+
+// object
+let Student = {
+    name: "Ahmed",
+    Rollon: 6,
+    city: "Sukkur"
+}
+
+console.log(Student)
+
+
