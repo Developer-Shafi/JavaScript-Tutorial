50606cdd4359afcbd8e992c1c8ef6b814f47db1f
Developer-Shafi <shafitechdeveloper1133@gmail.com>
2024-06-15 23:34:57 +0500

    TypeOf operator and Type Casting

--- DataTypes in JavaScript/index.js
+++ DataTypes in JavaScript/index.js
@@ -1,34 +1,45 @@
-// String, Number,BigInt,Null,undefined,Symbol,
-// let name = "Ahmed"
-// let Score = 444.3
-// let Bignumber = 32188588848893829447n
-// // let Temprature = Null
-// let gameCounter = undefined
-// let button = Symbol("123")
-// let loginbutton = Symbol("123")
-// console.log(Symbol("123") === Symbol("123"))
+// // String, Number,BigInt,Null,undefined,Symbol,
+// // let name = "Ahmed"
+// // let Score = 444.3
+// // let Bignumber = 32188588848893829447n
+// // // let Temprature = Null
+// // let gameCounter = undefined
+// // let button = Symbol("123")
+// // let loginbutton = Symbol("123")
+// // console.log(Symbol("123") === Symbol("123"))
 
-// Non primitive Datatypes in JavaScript
+// // Non primitive Datatypes in JavaScript
 
-// Object, Function, Array
+// // Object, Function, Array
 
-// Array
-let lovebirds = ["Muzamil,Khizra","humair,Malah","Yasir, Amal"]
+// // Array
+// let lovebirds = ["Muzamil,Khizra","humair,Malah","Yasir, Amal"]
 
-// Function
-const Addition =(a,b)=>{
-return a + b;
-}
-console.log(lovebirds)
-console.log(Addition(22,33))
+// // Function
+// const Addition =(a,b)=>{
+// return a + b;
+// }
+// console.log(lovebirds)
+// console.log(Addition(22,33))
 
-// object
-let Student = {
-    name: "Ahmed",
-    Rollon: 6,
-    city: "Sukkur"
-}
+// // object
+// let Student = {
+//     name: "Ahmed",
+//     Rollon: 6,
+//     city: "Sukkur"
+// }
 
-console.log(Student)
+// console.log(Student)
 
+// =======================Typeof Operator===============
+let name;
+let Gameover = true
+let AhmedBankBalace = 3987385738947284938849n
+let Score = 44.3
 
+console.log(typeof(name))
+console.log(typeof(Gameover))
+console.log(typeof(AhmedBankBalace))
+console.log(typeof(Score))
+
+// console.table([typeof(name),typeof(Gameover),typeof(AhmedBankBalace),typeof(Score)])
---
+++ Type_Casting.js/index.js
@@ -0,0 +1,28 @@
+// let a = "1"
+// let b = 1
+// console.log(a + b);
+// // console.log(1+"1")
+// let str = "123";
+// let num = Number(str); // Explicitly converts the string to a number
+// console.log(num); // 123
+// console.log(typeof(num))
+
+let num = 123;
+let Score = null + num;
+// let str = "The number is " + Score; // Implicitly converts the number to a string
+// console.log(str); // "The number is 123"
+// console.log(typeof(str))
+// console.log(typeof(num))
+// console.log(Score)
+// console.log(typeof(Score))
+
+// let GameOver = undefined + null;
+// console.log(GameOver)
+// console.log(typeof(GameOver))
+
+
+let a = "22"
+let b = 33
+let c = a + b
+console.log(c)
+console.log(typeof(c))
