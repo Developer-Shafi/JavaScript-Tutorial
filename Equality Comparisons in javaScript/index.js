296612ce87a653e26ebe188d8d309d1149a397c5
Developer-Shafi <shafitechdeveloper1133@gmail.com>
2024-06-26 23:25:52 +0500

    Learn about equality comparsions in javaScript

---
+++ Equality Comparisons in javaScript/index.js
@@ -0,0 +1,13 @@
+// let AhmedScore = NaN
+// let ShafiScore = NaN
+
+// console.log(Object.is(AhmedScore, ShafiScore))
+// console.log(AhmedScore === ShafiScore)
+
+let Addone = {}
+let Addtwo = []
+
+console.log(Object.is(Addone, Addtwo))
+
+console.log(isNaN(parseFloat("geoff"))); // true
+isNaN(parseFloat("10geo")); // false
