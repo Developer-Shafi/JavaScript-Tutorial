3c9ca46521dee4f41f34773b44f5ddb981678148
Developer-Shafi <shafitechdeveloper1133@gmail.com>
2024-07-06 23:50:46 +0500

    For Break continue statements

---
+++ Iterrations or Loops in js/For_loop.js
@@ -0,0 +1,47 @@
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// // }
+// for (let i = 1; i <= 10; i++){
+//     // const Values = i
+//     console.log(i)
+// }
+// let counter = 1
+// for(counter; counter <= 5; counter++){
+//     console.log(counter)
+// }
+
+// for(Table_number = 1; Table_number <= 10; Table_number++){
+//     console.log(`Now we print the table of ${Table_number}`)
+//     for(number= 1; number <= 10; number++){
+//         console.log(`${Table_number} X ${number} = ${Table_number * number}`)
+//     }
+// }
+
+// const fruits = ["Mango","Apple","Orange"]
+// for(let I = 0; I < fruits.length; I++){
+//     console.log(fruits[I])
+// }
+
+// Break keword in javaScript 
+
+// for(let num = 1; num <= 10; num++){
+//     if(num === 5){
+//         console.log("Number 5 is Occured")
+//         break
+//     }
+//     console.log(num)
+// }
+
+// Continue statment in javaScript
+
+// for(let num = 1; num <= 10; num++){
+//     if(num === 3){
+//         continue
+//     }
+//     console.log(num)
+// }
+
+
+
+
