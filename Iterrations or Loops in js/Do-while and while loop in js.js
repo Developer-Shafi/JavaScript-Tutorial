6cfa1534cc1b2609841c3b5b4d591826fc6acdfe
Developer-Shafi <shafitechdeveloper1133@gmail.com>
2024-07-07 23:44:24 +0500

    Today we cover while and do while

---
+++ Iterrations or Loops in js/Do-while and while loop in js.js
@@ -0,0 +1,16 @@
+// for(let i = 1; i <= 10; i++){
+//     console.log(i)
+// }
+
+// let i = 1;
+// while (i<=10) {
+//     console.log(i)
+//     i++
+// }
+
+// Do While loop in javaScript
+let j = 1;
+do {
+    console.log(j)
+    // j++
+} while (j > 1);
